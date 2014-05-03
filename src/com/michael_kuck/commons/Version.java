727c5403bb60e765feb870391cb35893c7e0045f
Michael <me@michael-kuck.com>
2014-05-03 21:25:22 +0800

    Initial Commit

---
+++ src/com/michael_kuck/commons/Version.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.michael_kuck.commons;
+
+/**
+ * @author michaelkuck
+ *
+ */
+public class Version {
+
+	final public static int MAJOR = 0;
+	final public static int MINOR = 1;
+	final public static int PATCH = 1;
+	
+	
+}
