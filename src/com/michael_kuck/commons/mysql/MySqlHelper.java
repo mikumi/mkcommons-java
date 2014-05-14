d0dfab535eb9f221df4a40b1bfb5563f3233caba
Michael <me@michael-kuck.com>
2014-05-14 13:31:34 +0800

    v1.2.4 - Removed MySqlConnectionPool as it didn't perform as desired and publicly available library BoneCP is much better - MySqlHelper can now safely close connections without having to check for null or catch exceptions all the time.

--- src/com/michael_kuck/commons/mysql/MySqlHelper.java
+++ src/com/michael_kuck/commons/mysql/MySqlHelper.java
@@ -26,33 +26,47 @@ public class MySqlHelper {
 	/**
 	 * @return
 	 */
-    public static Connection getNewConnection(final String host, final int port, final String user,
-                                              final String password, final String database)
+    public static Connection getNewConnection(final String host, final int port, final String user, final String password, final String database) throws
+                                                                                                                                                  SQLException
     {
         Connection connection = null;
 
         final String connectionUrl = getUrl(host, port, database);
         Properties connectionProperties = new Properties();
+        connectionProperties.put("autoReconnnect", "true");
         connectionProperties.put("user", user);
         connectionProperties.put("password", password);
-        connectionProperties.put("autoReconnnect", "true");
         Log.debug("Trying to connect to " + connectionUrl + " with user: " + user + ", password: (hidden)");
-        try
-        {
-            connection = DriverManager.getConnection(connectionUrl, connectionProperties);
-        } catch (final SQLException e)
-        {
-            Log.error("Error connecting to MySQL database: " + e.getLocalizedMessage());
-        }
+        connection = DriverManager.getConnection(connectionUrl, connectionProperties);
 
         return connection;
     }
 
-	/**
-	 * @return
-	 */
-    private static String getUrl(final String host, final int port, final String database) {
+    /**
+     * @param host
+     * @param port
+     * @param database
+     * @return
+     */
+    public static String getUrl(final String host, final int port, final String database) {
         return "jdbc:mysql://" + host + ":" + port + "/" + database;
+    }
+
+    /**
+     * Safely closes a connection if applicable.
+     * <p>
+     * Checks for null. Catches exceptions.
+     *
+     * @param connection
+     */
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                Log.error("Could not close connection: " + e.getLocalizedMessage());
+            }
+        }
     }
 
 }
